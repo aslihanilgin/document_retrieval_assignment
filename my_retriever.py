2c6ba78953c1caebca6d50e8b10f2460fd6bcde4
aca19aio <aiokan1@sheffield.ac.uk>
2022-10-31 16:32:36 +0000

    implemented reconstruct_index and initialised different term weighting methods

--- my_retriever.py
+++ my_retriever.py
@@ -1,4 +1,5 @@
 
+from bdb import set_trace
 from cmath import sqrt
 import math
 from operator import index
@@ -11,16 +12,38 @@ class Retrieve:
         self.index = index
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
+        self.reconstruct_index(self.index)
         
     def compute_number_of_documents(self):
         self.doc_ids = set() 
         for term in self.index:
             self.doc_ids.update(self.index[term])
+        # # debug
+        # print("self.doc_ids: {}".format(self.doc_ids))
 
         self.number_of_documents = len(self.doc_ids)
+
+
+    def reconstruct_index(self, initial_index):
+        # initiliase the keys as document ids
+        reconstructed_index = dict.fromkeys(self.doc_ids, None)
         # debug
-        # print("type(self.number_of_documents): {}".format(type(self.number_of_documents)))
-        # return self.number_of_documents
+        # import pdb; pdb.set_trace()
+
+        for term, doc_id_tf_values_dict in self.index.items():
+            for doc_id, tf_value in doc_id_tf_values_dict.items():
+                # debug
+                # print("doc_id_tf_values_dict.items(): {}".format(doc_id_tf_values_dict.items()))
+                reconstructed_index.update({doc_id: {term: tf_value}})
+               
+        # debug
+        print("reconstructed_index: {}".format(reconstructed_index))
+
+    def binary_term_weighting_computation(self, index):
+
+    def tf_term_weighting_computation(self, index):
+
+    def tfidf_term_weighting_computation(self, index):
 
 
     # Method performing retrieval for a single query (which is 
@@ -28,7 +51,19 @@ class Retrieve:
     # of doc ids for relevant docs (in rank order).
     def for_query(self, query):
 
-        self.compute_doc_vector_size('hehe', query)
+        if self.term_weighting = 'binary':
+            binary_term_weighting_computation()
+
+        elif self.term_weighting = 'term_frequency':
+            tf_term_weighting_computation()
+
+        elif self.term_weighting = 'tfidf':
+            tfidf_term_weighting_computation()
+        else:
+            binary_term_weighting_computation()
+
+       
+
 
         return list(range(1,11))
 
