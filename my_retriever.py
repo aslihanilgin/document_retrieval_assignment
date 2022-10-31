2b0303ddb9d514efd5a11af6f133e039664ad73c
aca19aio <aiokan1@sheffield.ac.uk>
2022-10-31 12:25:03 +0000

    Created several methods for several computations

--- my_retriever.py
+++ my_retriever.py
@@ -1,5 +1,7 @@
 
+from cmath import sqrt
 import math
+from operator import index
 
 class Retrieve:
     
@@ -9,8 +11,6 @@ class Retrieve:
         self.index = index
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
-        self.all_unique_query_words = set()
-        self.query_words_and_their_idf = dict()
         
     def compute_number_of_documents(self):
         self.doc_ids = set() 
@@ -21,65 +21,108 @@ class Retrieve:
         # debug
         # print("type(self.number_of_documents): {}".format(type(self.number_of_documents)))
         # return self.number_of_documents
-    
-    def compute_unique_query_words(self, query):
-        for query_word in query:
-            self.all_unique_query_words.add(query_word)
+
+    def compute_idf(doc_no, doc_freq):
+        return math.log((doc_no/doc_freq))
 
     def compute_idf_for_all_query_words(self, query):
-
-        # compute how many documents the query word appears in:
+        self.unique_query_words = set()
+        self.query_words_and_their_idf = dict()
+        for query_word in query:
+            self.unique_query_words.add(query_word)
+            # compute how many documents the query word appears in:
         
-        for query_word in self.all_unique_query_words:
-            # if query word appears in any of the documents
+        for query_word in self.unique_query_words:
+            # look through the index keys
             if query_word in self.index:
             # find the length of that key's value (how many entries
             # that key has)
                 doc_frequency = len(self.index[query_word])
-                idf_value_for_query_word = math.log(self.number_of_documents / doc_frequency)
+                # debug
+                # print("self.doc_frequency: {}".format(self.doc_frequency))
+                # debug
+                print("type(self.number_of_documents): {}".format(type(self.number_of_documents)))
+                idf_value_for_query_word = self.compute_idf(self.number_of_documents, doc_frequency)
                 self.query_words_and_their_idf[query_word] = idf_value_for_query_word
-            else:
-                # query word does not appear in any of the documents
-                self.query_words_and_their_idf[query_word] = 0
-    
-    # def compute_term_weighting(self, query, doc_id, tf, idf):
-    def compute_term_weighting(self, query):
-
-        # term frequency dict
-        term_frequencies = dict()
-
-        # compute tf . idf value table for the query 
-        for query_word in self.all_unique_query_words:
-            # compute term frequency
-            term_frequencies[query_word] = query.count(query_word)
-            tf_idf_of_query_word = term_frequencies[query_word] * self.query_words_and_their_idf[query_word]
-            # debug
-            print("tf_idf_of_query_word: {}".format(tf_idf_of_query_word))
-
         # debug
-        # print("term_frequencies: {}".format(term_frequencies))
+        print("self.query_words_and_their_idf: {}".format(self.query_words_and_their_idf))
+
+    def length_of_vector_equation(self, term_weighting_values):
+        sum = 0
+        for value in term_weighting_values:
+            sum += pow(value, 2)
+        return math.sqrt(sum)
+
+    def compute_doc_vector_size(self, term_weighting_choice, query):        
+        # if term_weighting_choice == 'term_frequency':
+            
+        # elif term_weighting_choice == 'tfidf':
+
+        # else:
+            # binary 
+
+            # get only unique terms from query
+            unique_terms_in_query = list(set(query))
+                
+            binary_values_for_query_terms = dict() # term -> binary value
+
+            # creating binary values dict for query terms
+            for term in unique_terms_in_query:
+                if term in self.index:
+                    binary_values_for_query_terms[term] = 1
+                else: 
+                    binary_values_for_query_terms[term] = 0
+            
+            length_of_vector_for_query = self.length_of_vector_equation(binary_values_for_query_terms.values())
+            
+            # creating sum of tf of each document
+
+            # TODO: rename
+            doc_id_to_tf_dict = dict.fromkeys(self.doc_ids, 0)
+
+            for doc_id in self.doc_ids:
+                # TODO: maybe order doc_ids? 
+
+                # TODO: retrieve from self.index / 
+                # retrieve doc_id -> count ---- for each count do length_of_vector_equation
+                for term_values in self.index:
+                    if doc_id in self.index[term_values]:
+                        # debug
+                        print("term_values[doc_id]: {}".format(self.index[term_values][doc_id]))
+                        doc_id_to_tf_dict[doc_id] = term_values[doc_id]
+                    else:
+                        continue
+                # debug
+                # print("\nself.index.values()[doc_id]: {}".format(list(self.index.values())[0]))
+                
+                length_of_vector_for_doc = self.length_of_vector_equation(doc_id_to_tf_dict)
+
+            # debug
+            print("doc_id_to_tf_dict: {}".format(doc_id_to_tf_dict))
+
+
+
+
 
 
     # Method performing retrieval for a single query (which is 
     # represented as a list of preprocessed terms). Returns list 
     # of doc ids for relevant docs (in rank order).
     def for_query(self, query):
-        
+
+        self.compute_doc_vector_size('hehe', query)
+
+        # self.compute_idf_for_all_query_words(query)
+
         # debug
         # print("query: {}".format(query))
 
-        self.compute_unique_query_words(query)
+        # term frequency calculation (tf)
+        # if self.term_weighting == 'binary':
         
-        # debug
-        # print("self.all_unique_query_words: {}".format(self.all_unique_query_words))
-
-        self.compute_idf_for_all_query_words(self.all_unique_query_words)
-
-        # debug
-        # print("self.query_words_and_their_idf: {}".format(self.query_words_and_their_idf))
-
-        self.compute_term_weighting(query)
-        
+        # inverse document frequency calculation (idf)
+        # tf.idf term weighting calculation (tf.idf)
+        # elif self.term_weighting == 'tfidf':
 
         return list(range(1,11))
 
