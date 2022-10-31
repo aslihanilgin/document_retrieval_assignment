62d970d856f10c2c0bb2059564edaf4d4dd2f6bd
aca19aio <aiokan1@sheffield.ac.uk>
2022-10-31 18:27:08 +0000

    implemented finding common words between query and each doc, not recording tf

--- my_retriever.py
+++ my_retriever.py
@@ -12,7 +12,7 @@ class Retrieve:
         self.index = index
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
-        self.reconstruct_index(self.index)
+        self.reconstructed_index = self.reconstruct_index(self.index)
         
     def compute_number_of_documents(self):
         self.doc_ids = set() 
@@ -37,7 +37,8 @@ class Retrieve:
                 reconstructed_index.update({doc_id: {term: tf_value}})
                
         # debug
-        print("reconstructed_index: {}".format(reconstructed_index))
+        # print("reconstructed_index: {}".format(reconstructed_index))
+        return reconstructed_index
 
     def length_of_vector_equation(self, term_weighting_values):
         sum = 0
@@ -45,30 +46,98 @@ class Retrieve:
             sum += pow(value, 2)
         return math.sqrt(sum)
 
-    def binary_term_weighting_computation(self, index):
+    def binary_term_weighting_computation(self, query):
+        # get only unique terms from query
+        unique_terms_in_query = set(query)
+        doc_terms = set()
+        for doc, term_and_tf_dict in self.reconstructed_index.items():
+            [doc_terms.add(term) for term, tf in term_and_tf_dict.items()]
+            common_terms = unique_terms_in_query.intersection(set(doc_terms))
+            # debug
+            if len(common_terms) > 0:
+                print("doc: {}, query: {}, \ncommon_terms: {}\n".format(doc, unique_terms_in_query, common_terms))
 
-    def tf_term_weighting_computation(self, index):
+    # def tf_term_weighting_computation(self, index, query):
 
-    def tfidf_term_weighting_computation(self, index):
+    # def tfidf_term_weighting_computation(self, index, query):
 
 
     # Method performing retrieval for a single query (which is 
     # represented as a list of preprocessed terms). Returns list 
     # of doc ids for relevant docs (in rank order).
     def for_query(self, query):
+        #debug
+        self.binary_term_weighting_computation(query)
 
-        if self.term_weighting == 'binary':
-            self.binary_term_weighting_computation()
+        # if self.term_weighting == 'binary':
+        #     self.binary_term_weighting_computation()
 
-        elif self.term_weighting == 'term_frequency':
-            self.tf_term_weighting_computation()
+        # elif self.term_weighting == 'tf':
+        #     self.tf_term_weighting_computation()
 
-        elif self.term_weighting == 'tfidf':
-            self.tfidf_term_weighting_computation()
-        else:
-            self.binary_term_weighting_computation()
+        # elif self.term_weighting == 'tfidf':
+        #     self.tfidf_term_weighting_computation()
+        # else:
+        #     self.binary_term_weighting_computation()
 
        
 
 
         return list(range(1,11))
+
+
+################################
+
+def length_of_vector_equation(self, term_weighting_values):
+        sum = 0
+        for value in term_weighting_values:
+            sum += pow(value, 2)
+        return math.sqrt(sum)
+
+def compute_doc_vector_size(self, term_weighting_choice, query):        
+    # if term_weighting_choice == 'term_frequency':
+        
+    # elif term_weighting_choice == 'tfidf':
+
+    # else:
+        # binary 
+
+        # get only unique terms from query
+        unique_terms_in_query = list(set(query))
+            
+        binary_values_for_query_terms = dict() # term -> binary value
+
+        # creating binary values dict for query terms
+        for term in unique_terms_in_query:
+            if term in self.index:
+                binary_values_for_query_terms[term] = 1
+            else: 
+                binary_values_for_query_terms[term] = 0
+        
+        length_of_vector_for_query = self.length_of_vector_equation(binary_values_for_query_terms.values())
+        
+        # creating sum of tf of each document
+
+        # TODO: rename
+        doc_id_to_tf_dict = dict.fromkeys(self.doc_ids, 0)
+
+        for doc_id in self.doc_ids:
+            # TODO: maybe order doc_ids? 
+
+            # TODO: retrieve from self.index / 
+            # retrieve doc_id -> count ---- for each count do length_of_vector_equation
+            for term_values in self.index:
+                if doc_id in self.index[term_values]:
+                    # debug
+                    print("term_values[doc_id]: {}".format(self.index[term_values][doc_id]))
+                    doc_id_to_tf_dict[doc_id] = term_values[doc_id]
+                else:
+                    continue
+            # debug
+            # print("\nself.index.values()[doc_id]: {}".format(list(self.index.values())[0]))
+            
+            length_of_vector_for_doc = self.length_of_vector_equation(doc_id_to_tf_dict)
+
+        # debug
+        print("doc_id_to_tf_dict: {}".format(doc_id_to_tf_dict))
+
